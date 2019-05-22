2c2312766697ee0edf591af104438b0f38c4abec
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-22 18:54:13 +0100

    Create carousel for mobile and include hammer.js functionality

--- Components/plans/plans.js
+++ Components/plans/plans.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 class Plan {
   constructor(planElement) {
     this.planElement = planElement;
@@ -22,3 +23,50 @@ class Plan {
 
 const plans = document.querySelectorAll('.price-plans');
 plans.forEach(plan => new Plan(plan));
+
+const plansArray = [...plans];
+const container = document.querySelector('.plans-articles-container');
+
+class Carousel {
+  constructor(carouselElement) {
+    this.carouselElement = carouselElement;
+    this.plan = plansArray;
+    this.currentIndex = 0;
+    this.container = container;
+    this.active();
+    const swipeFn = new Hammer(container);
+    swipeFn.on('swipeleft', this.swipeLeft.bind(this));
+    swipeFn.on('swiperight', this.swipeRight.bind(this));
+  }
+
+  active() {
+    this.plan[this.currentIndex].classList.add('active');
+  }
+
+  resetActive() {
+    this.plan.forEach(plan => plan.classList.remove('active'));
+  }
+
+  swipeLeft() {
+    this.resetActive();
+    if (this.currentIndex === 0) {
+      this.plan[this.plan.length - 1].classList.add('active');
+      this.currentIndex = this.plan.length - 1;
+    } else {
+      this.plan[--this.currentIndex].classList.add('active');
+    }
+  }
+
+  swipeRight() {
+    this.resetActive();
+    if (this.currentIndex === this.plan.length - 1) {
+      this.plan[0].classList.add('active');
+      this.currentIndex = 0;
+    } else {
+      this.plan[++this.currentIndex].classList.add('active');
+    }
+  }
+}
+
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(carousel => new Carousel(carousel));
