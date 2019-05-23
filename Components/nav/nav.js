c68030b93b3aec067fb294b5626899d85706f2f7
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-23 08:12:06 +0100

    Add menu animations at small screen sizes using GSAP

--- Components/nav/nav.js
+++ Components/nav/nav.js
@@ -1,13 +1,27 @@
 const menu = document.querySelector('nav');
+const maxWidth = window.matchMedia('(max-width: 800px)');
+const widthFn = () => {
+  if (!maxWidth.matches) {
+    menu.style.height = 'inherit';
+  }
+};
+
 const toggleMenu = () => {
   if (menu.style.display === 'flex') {
-    menu.style.display = 'none';
+    // menu.style.display = 'none';
+    TweenMax.fromTo(menu, 0.5, { opacity: 1 }, { opacity: 0, height: 0, display: 'none' });
   } else {
-    menu.style.display = 'flex';
+    // menu.style.display = 'flex';
+    TweenMax.fromTo(menu, 0.5, { opacity: 0 }, { opacity: 1, height: 350, display: 'flex' });
   }
 };
 
 const hamburger = document.querySelector('.fa-bars');
 hamburger.addEventListener('click', () => {
   toggleMenu();
+});
+
+// Resets nav height when swapping from mobile/tablet/desktop view.
+window.addEventListener('resize', () => {
+  widthFn();
 });
